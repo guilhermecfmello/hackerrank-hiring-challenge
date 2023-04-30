c329c12009e28d5b9cd9011a69c7ad8a86e13131
Guilherme Mello <guilhermecfmello@gmail.com>
2023-04-29 21:43:59 -0300

    remove useless code from binary search tree solution

--- ctci-is-binary-search-tree/src/main.cpp
+++ ctci-is-binary-search-tree/src/main.cpp
@@ -11,8 +11,6 @@ The Node struct is defined as follows:
 	bool checkBST(Node* root) {
         if(root == NULL)
             return true;
-        // bool result = checkLeft(root->left, root->data) && checkRight(root->right, root->data);
-        // cout << "Result: " << result << "\n";
         return check(root, INT_MIN, INT_MAX);
     }
 
@@ -24,25 +22,5 @@ The Node struct is defined as follows:
                 check(root->left, min, root->data) && check(root->right, root->data, max)
             );
         }
-        return false;
-    }
-
-    bool checkLeft(Node *root, int value) {
-        if(root == NULL) {
-            return true;
-        }
-        // cout << "comparing if " << root->data << " < " << value << "\n";
-        if(root->data < value)
-            return (checkLeft(root->left, root->data) && checkRight(root->right, root->data));
-        return false;
-    }
-
-    bool checkRight(Node *root, int value) {
-        if(root == NULL) {
-            return true;
-        }
-        // cout << "comparing if " << root->data << " > " << value << "\n";
-        if(root->data > value)
-            return (checkLeft(root->left, root->data) && checkRight(root->right, root->data));
         return false;
     }
